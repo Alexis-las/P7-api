f315471f163cf37116600984e5b965b79d398cc5
Alexis-las <alexis.soubien@free.fr>
2023-10-10 17:31:08 +0200

    First functional code commit

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, jsonify, request
 
 from P7_Scoring.input import load_data, load_model
 
+import lime
+from lime import lime_tabular
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -51,7 +54,25 @@ def verif_client_id(client_id):
 def get_score(id: int):
     if verif_client_id(id):
         score = model.predict_proba(data.loc[data.index == id].drop(columns=['TARGET']))
-        return jsonify(round(score[0][1]*100,2))
+        return jsonify({'score': round(score[0][1]*100,2)}), 200
+    else:
+        return jsonify({'error': 'Client does not exist'}), 404
+
+
+# Explanation LIME
+@app.route('/client_explain/<int:id>', methods=['GET'])
+def get_explanation(id: int):
+    if verif_client_id(id):
+        explainer = lime_tabular.LimeTabularExplainer(data.drop(columns=['TARGET']),
+                                                      mode="classification",
+                                                      feature_names=data.drop(columns=['TARGET']).columns,
+                                                      discretize_continuous=False,
+                                                      verbose=True
+                                                     )
+        explanation = explainer.explain_instance(data.drop(columns=['TARGET']).loc[id].values,
+                                                 model.predict_proba,
+                                                 num_features=10)
+        return (explanation.as_html()), 200
     else:
         return jsonify({'error': 'Client does not exist'}), 404
 

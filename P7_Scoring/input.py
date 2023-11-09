23a4c61a4b2f6d71adf5dedce58b59a0bb297b48
Alexis-las <alexis.soubien@free.fr>
2023-11-09 15:19:27 +0100

    Final functional code commit

--- P7_Scoring/input.py
+++ P7_Scoring/input.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import pickle
+import os
 # from P7_Scoring.custom_metrics import business_income
 # from P7_Scoring.custom_metrics import business_cost
 
 
 def load_data(path="input/app_train_final.csv"):
     # Import data
-    data = pd.read_csv(path, sep=',', index_col='SK_ID_CURR', low_memory=False)
+    full_path = os.path.abspath(path)
+    data = pd.read_csv(full_path, sep=',', index_col='SK_ID_CURR', low_memory=False)
     return data
 
 

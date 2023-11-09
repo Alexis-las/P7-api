a8c1a41b57b978590c844d9d42f8066a4e044dbe
Alexis-las <alexis.soubien@free.fr>
2023-11-09 13:50:51 +0100

    Final functional code commit

--- P7_Scoring/input.py
+++ P7_Scoring/input.py
@@ -4,12 +4,12 @@ import pickle
 # from P7_Scoring.custom_metrics import business_cost
 
 
-def load_data(path="input/app_train_final_20_20K.csv"):
+def load_data(path="input/app_train_final.csv"):
     # Import data
     data = pd.read_csv(path, sep=',', index_col='SK_ID_CURR', low_memory=False)
     return data
 
 
-def load_model(path="input/modele_LR_business_cost_new_pipe.pickle"):
+def load_model(path="input/modele_Final_LR_business_cost.pickle"):
 #def load_model(path="input/modele_LR_business_score.pickle"):
     return pickle.load(open(path, 'rb'))
